a38ffe50aa5b999a57909e87c506cd95a93e4984
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-03 18:47:54 +0530

    Testing post gen bash wagtail hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,4 +5,8 @@ set -euxv
 echo "Post cookiecutter project generation"
 echo "Doing wagtail related stuff"
 
+echo "PWD: $PWD"
+
+ls
+
 cp ../wagtail/default_apps/* {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/ -ruv
