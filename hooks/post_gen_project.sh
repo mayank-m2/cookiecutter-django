ab4b9ddc36246cd4e5930122f949e5324063f579
Mayank Mahajan <mayankm2089@gmail.com>
2023-08-03 18:47:03 +0530

    Fixed post gen bash wagtail hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,4 +5,4 @@ set -euxv
 echo "Post cookiecutter project generation"
 echo "Doing wagtail related stuff"
 
-cp wagtail/default_apps/* {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/ -ruv
+cp ../wagtail/default_apps/* {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/ -ruv
